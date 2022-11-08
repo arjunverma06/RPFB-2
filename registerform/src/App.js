fbf5dceb35dbf3c613ddb504c31dcbdcb26ff723
arjunverma06 <arjunverma5319@gmail.com>
2022-11-08 15:11:53 +0530

    random

--- registerform/src/App.js
+++ registerform/src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <RegistrationForm/>  */}
-      <Login/>
-      {/* <Signup/> */}
+     { /*<RegistrationForm/>*/}
+    {<Login/>}
+      { /*<Signup/> */}
       <Profile/>
     </div>
   );
